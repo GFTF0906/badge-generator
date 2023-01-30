0a3eed2c7828007ef4f04e49a1d0bfc65fe71d7c
Guilherme Fontebasso <guitavaresgames@gmail.com>
2023-01-30 13:01:42 -0300

    Changes: fixed error paragraph error

--- src/components/Error/ErrorParagraph.tsx
+++ src/components/Error/ErrorParagraph.tsx
@@ -1,5 +1,5 @@
-import { THeadings } from '../../types/HeadingsType';
+import { TElementWithContent } from '../../types/ElementWithContent';
 
-export const ErrorParagraph = ({ text: message }: THeadings) => {
+export const ErrorParagraph = ({ text: message }: TElementWithContent) => {
   return <p className="text-red-500 text-base">{message}</p>;
 };

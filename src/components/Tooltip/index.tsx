d36d05e42d68e7e339263e1479dd22c2cbddfc0d
Guilherme Fontebasso <guitavaresgames@gmail.com>
2023-02-01 22:18:59 -0300

    Changes: removed tooltip view in small screens

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -8,7 +8,7 @@ export const Tooltip = ({
   return isHovered ? (
     <div
       aria-label={tip}
-      className="absolute -right-20 top-12 z-20 w-60 rounded border-2 border-gray-500 bg-gray-700 p-6 font-bold text-[#F5F5F5]"
+      className="absolute -right-20 top-12 z-20 hidden w-60 rounded border-2 border-gray-500 bg-gray-700 p-6 font-bold text-[#F5F5F5] lg:block"
     >
       <h3>{tip}</h3>
     </div>
